a6c1e89baf02413d142f97ee5e077e517e56b54f
linksynapse <kailnet120@linksynapse.com>
2021-10-22 21:01:04 +0800

    파이널 릴리즈

--- fstsys.py
+++ fstsys.py
@@ -6,21 +6,20 @@ import terminal as term
 #import terminal as tm
 
 lock = threading.Lock()
-#x = threading.Thread(target=EASys.eAccountsycExec, args=[lock,])
-#y = threading.Thread(target=EHSys.eHistorysycExec, args=[lock,])
+x = threading.Thread(target=EASys.eAccountsycExec, args=[lock,])
+y = threading.Thread(target=EHSys.eHistorysycExec, args=[lock,])
 z = threading.Thread(target=term.Read, args=[lock,])
 
-try:
-    z.start()
 
-    while True:
+z.start()
+while True:
+    try:
         print('Start Sync Account')
         EASys.eAccountsycExec(lock)
         print('start sync History')
         EHSys.eHistorysycExec(lock)
-        #x.start()
-        #y.start()
+        x.start()
+        y.start()
+        time.sleep(15)
+    except:
         time.sleep(30)
-
-except:
-    time.sleep(15)
